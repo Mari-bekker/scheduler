b804d580ae3b3823a55bb2b4ae168eebcea49ddc
Marina Bekker <mshooch@gmail.com>
2022-05-24 19:47:35 -0400

    implemnent the interviewer list item with classname

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -6,14 +6,18 @@ import classNames from "classnames";
 
 export default function InterviewerListItem(props) {
 
+    const interviewerListItemClass = classNames("interviewers__item", {
+    "interviewers__item--selected": props.selected,
+  });
+
   return (
-<li className="interviewers__item">
-  <img
-    className="interviewers__item-image"
-    src="https://i.imgur.com/LpaY82x.png"
-    alt="Sylvia Palmer"
-  />
-  Sylvia Palmer
-</li>
+  <li className ={interviewerListItemClass} selected={props.selected}  onClick={() => props.setInterviewer(props.id)}>
+    <img
+      className="interviewers__item-image"
+      src="https://i.imgur.com/LpaY82x.png"
+      alt="Sylvia Palmer"
+    />
+    Sylvia Palmer
+  </li>
   );
 }
