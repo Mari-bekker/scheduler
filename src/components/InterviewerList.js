431c8b6eca3d73238de7511ca1096d7cb0eb0021
Marina Bekker <mshooch@gmail.com>
2022-05-24 23:39:02 -0400

    wip on interviewer list

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -4,12 +4,26 @@ import classNames from "classnames";
 
 import "components/InterviewerList.scss";
 
-export default function InterviewerList(props) {
+import InterviewerListItem from "./InterviewerListItem";
 
-return (
-<section className="interviewers">
-  <h4 className="interviewers__header text--light">Interviewer</h4>
-  <ul className="interviewers__list"></ul>
-</section>
+export default function InterviewerList(props) {
+  const interviewers = props.interviewers.map( interviewer => {
+    return (
+      <InterviewerListItem
+        key={interviewer.id}
+        name={interviewer.name}
+        avatar={interviewer.avatar}
+        setinterviewer={props.setDay}
+        interviewer={interviewer.id === props.interviewer}
+        />
+    );
+  })
+
+return ( <ul> {interviewers } </ul>
+// {/* <section className="interviewers">
+//   <h4 className="interviewers__header text--light">Interviewer</h4>
+//   <ul className="interviewers__list"></ul>
+// </section> */}
+// );
 );
 }
