84c23cdfd789c59eae4167790282420ec64143be
Marina Bekker <mshooch@gmail.com>
2022-05-30 21:39:20 -0400

    add get appointment for day selector

---
+++ src/helpers/selectors.js
@@ -0,0 +1,14 @@
+export function getAppointmentsForDay(state, name) {
+  const filteredDays = state.days.filter(day => day.name === name);
+  if(state.days.length===0 || filteredDays.length===0){
+    return [];
+  }
+  const dayAppointments = filteredDays[0].appointments;
+ 
+  let filteredAppointments = [];
+ 
+  for(let appointment of dayAppointments) {
+    filteredAppointments.push(state.appointments[appointment]);
+  }
+  return filteredAppointments;
+}
