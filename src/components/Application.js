71756e586c5eaf63725ec1c6e01c0d0557f6dfa5
Marina Bekker <mshooch@gmail.com>
2022-05-30 23:08:00 -0400

    use function to creat ethe daily appointments array to display them

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,46 +4,7 @@ import axios from "axios";
 import "components/Application.scss";
 import DayList from "components/DayList";
 import Appointment from "components/Appointment";
-
-// const appointments = {
-//   "1": {
-//     id: 1,
-//     time: "12pm",
-//   },
-//   "2": {
-//     id: 2,
-//     time: "1pm",
-//     interview: {
-//       student: "Lydia Miller-Jones",
-//       interviewer:{
-//         id: 3,
-//         name: "Sylvia Palmer",
-//         avatar: "https://i.imgur.com/LpaY82x.png",
-//       }
-//     }
-//   },
-//   "3": {
-//     id: 3,
-//     time: "2pm",
-//   },
-//   "4": {
-//     id: 4,
-//     time: "3pm",
-//     interview: {
-//       student: "Archie Andrews",
-//       interviewer:{
-//         id: 4,
-//         name: "Cohana Roy",
-//         avatar: "https://i.imgur.com/FK8V841.jpg",
-//       }
-//     }
-//   },
-//   "5": {
-//     id: 5,
-//     time: "4pm",
-//   }
-// };
-
+import getAppointmentsForDay from "helpers/selectors";
 
 
 export default function Application(props) {
@@ -53,8 +14,6 @@ export default function Application(props) {
     days: [],
     appointments: {}
   });
-
-  const dailyAppointments = [];
 
   const setDay = day => setState({ ...state, day });
 
@@ -68,6 +27,8 @@ export default function Application(props) {
       setState(prev => ({...prev, days, appointments }));
     });
   }, []);
+
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
 
   const appointment = Object.values(dailyAppointments).map(appointment => {
     return (
