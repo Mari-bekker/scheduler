357fd59bb5b2a4eda51d824939283b7a30008bfd
Marina Bekker <mshooch@gmail.com>
2022-06-13 23:48:10 -0400

    add prop types to interviewer list

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,10 +1,14 @@
 import React from "react";
+import PropTypes from 'prop-types';
 
 import "components/InterviewerList.scss";
 
 import InterviewerListItem from "./InterviewerListItem";
 
 export default function InterviewerList(props) {
+  InterviewerList.propTypes = {
+    interviewers: PropTypes.array.isRequired
+  };
   const interviewersList = props.interviewers.map( interviewer => {
     return (
       <InterviewerListItem
